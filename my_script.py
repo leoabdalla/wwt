d94458128576631a8a14d9bc259ee6b7ca8b1b45
Leonardo Abdalla <leonardo.abdalla@la.logicalis.com>
2025-02-18 02:09:33 +0000

    Updated python script

---
+++ my_script.py
@@ -0,0 +1,5 @@
+# Python code which collects a name and displays a greeting
+echo '#!/usr/bin/env python' >> my_script.py
+echo '# This script displays a greeting' >> my_script.py
+echo 'name = input("What is your name? ")' >> my_script.py
+echo 'print(f"It is nice to meet you, {name})' >> my_script.py
